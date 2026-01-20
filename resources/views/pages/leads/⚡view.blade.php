44122c04f25e86b0a8676e7f4465f1862e32d2bb
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2026-01-20 15:13:59 +0100

    Update view

--- resources/views/pages/leads/⚡view.blade.php
+++ resources/views/pages/leads/⚡view.blade.php
@@ -4,10 +4,7 @@ use Livewire\Component;
 
 new class extends Component
 {
-    public $name;
-    public $company;
     public $email;
-    public $phone;
     public $website;
     public $template;
     public $notes;
@@ -15,10 +12,7 @@ new class extends Component
     public function mount($id)
     {
         $lead = \App\Models\Leads::find($id);
-        $this->name = $lead->name;
-        $this->company = $lead->company;
         $this->email = $lead->email;
-        $this->phone = $lead->phone;
         $this->website = $lead->website;
         $this->template = $lead->template_id;
         $this->notes = $lead->notes;
@@ -27,20 +21,15 @@ new class extends Component
     public function updateLead()
     {
         $this->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:leads,email',
-            'company' => 'nullable|string|max:255',
-            'phone' => 'nullable|string|max:20',
-            'website' => 'nullable|url|max:255',
+            'email' => 'required|email',
+            'website' => 'url|max:255|required',
             'template' => 'nullable|exists:templates,id',
             'notes' => 'nullable|string',
         ]);
 
         $lead = \App\Models\Leads::where('email', $this->email)->first();
         $lead->update([
-            'name' => $this->name,
-            'company' => $this->company,
-            'phone' => $this->phone,
+            'email' => $this->email,
             'website' => $this->website,
             'template_id' => $this->template,
             'notes' => $this->notes,
@@ -57,11 +46,8 @@ new class extends Component
     <flux:heading size="xl" class="mt-8">{{ __('Lead :company / :website', ['company' => $this->company, 'website' => str_replace(['https://', 'http://'], '', $this->website)]) }}</flux:heading>
     <flux:separator class="mb-4" />
     <form wire:submit.prevent="updateLead">
-        <flux:input wire:model="name" label="{{ __('Name') }}" class="mb-4" required />
-        <flux:input wire:model="company" label="{{ __('Company') }}" class="mb-4" />
         <flux:input wire:model="email" label="{{ __('Email') }}" class="mb-4" required />
-        <flux:input wire:model="phone" label="{{ __('Phone') }}" class="mb-4" />
-        <flux:input wire:model="website" label="{{ __('Website') }}" class="mb-4" />
+        <flux:input wire:model="website" label="{{ __('Website') }}" class="mb-4" required />
         <flux:select wire:model="status" label="{{ __('Status')}}" class="mb-4" required>
             <flux:select.option value="new">{{ __('New') }}</flux:select.option>
             <flux:select.option value="contacted">{{ __('Contacted') }}</flux:select.option>
