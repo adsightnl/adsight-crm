1fd9603e5de4c08075a59af0eb136564c7536839
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2026-01-21 13:56:53 +0100

    aa

--- resources/views/pages/leads/⚡view.blade.php
+++ resources/views/pages/leads/⚡view.blade.php
@@ -56,8 +56,8 @@ new class extends Component
             <flux:select.option value="new">{{ __('New') }}</flux:select.option>
             <flux:select.option value="traffic">{{ __('Low Traffic') }}</flux:select.option>
             <flux:select.option value="broken">{{ __('Broken') }}</flux:select.option>
-            <flux:select.option value="inactive">{{ __('Inactive') }}</flux:select.option>
-            <flux:select.option value="lost">{{ __('No interest / Not eligible') }}</flux:select.option>
+            <flux:select.option value="inactive">{{ __('Inactive /  Not eligible') }}</flux:select.option>
+            <flux:select.option value="lost">{{ __('No interest) }}</flux:select.option>
             <flux:select.option value="contacted">{{ __('Contacted') }}</flux:select.option>
             <flux:select.option value="qualified">{{ __('Interested') }}</flux:select.option>
 
